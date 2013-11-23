5f40a9c18474ec2570fc0388f04bc795a0f501df
K A <kami@itu.dk>
2013-11-23 11:26:27 +0100

    Set up solution

---
+++ src/TriangleProblem/Entieties/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleProblem.Entieties
+{
+    public class Actor
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ src/TriangleProblem/Utils/GraphManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleProblem.Entieties;
+
+namespace TriangleProblem.Utils
+{
+    public class GraphManager
+    {
+        public Graph Graph { get; set; }
+
+        public GraphManager(Graph graph)
+        {
+            Graph = graph;
+        }
+
+        //public IEnumerable<Actor> FindTreeActorrsThatPlayedInMostMovies()
+        //{
+
+        //}
+    }
+}
