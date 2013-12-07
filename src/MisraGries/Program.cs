982c9fec2cc5fd8e4f10dc8db7ae99256ec1a2a4
pkmetski <pkme@itu.dk>
2013-12-07 20:36:48 +0100

    misra gries algorithm implementation

---
+++ src/MisraGries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisraGries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] movs = new int[][] 
+            {                
+                new int[] {1,2,3,4,5},
+                new int[] {2,3,4,6},
+                new int[] {1,3,4},
+                new int[] {5,6},
+                new int[] {1,2,3,4,5,6},
+            };
+
+            MGAlgorithm algorithm = new MGAlgorithm(3);
+            foreach (var mov in movs) { algorithm.Process(mov); }
+
+            var v = algorithm.Cache.OrderByDescending(pr => pr.Value).First();
+        }
+    }
+}
